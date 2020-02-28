e3b8047771568b5e3d95e3f7914dc5cc97ef3744
Joshua Haertel <josh.haertel@gmail.com>
2020-02-27 20:31:20 -0700

    Create classes

---
+++ tictactoe/grid.py
@@ -0,0 +1,8 @@
+from tictactoe.space import Space
+from tictactoe.word import Word
+
+
+class Grid:
+    def __init__(self):
+        self.space = Space()
+        self.word = Word()
---
+++ tictactoe/tictactoe.py
@@ -0,0 +1,6 @@
+from tictactoe.grid import Grid
+
+
+class TicTacToe:
+    def __init__(self):
+        self.grid = Grid()
